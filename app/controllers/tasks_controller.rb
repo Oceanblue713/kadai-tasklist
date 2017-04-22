c31a433106e0ffa994824a2f40cf896d6c606320
Andy <amedamachip713@gmail.com>
2017-04-22 10:48:23 +0000

    fix usercont

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
     @tasks = Task.all
@@ -28,7 +28,7 @@ class TasksController < ApplicationController
   end
   
   def edit
-    @task = current_user.tasks.find(params[:id])
+    @task = Task.find(params[:id])
   end
   
   def update
